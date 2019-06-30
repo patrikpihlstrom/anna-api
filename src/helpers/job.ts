feb73f730527351310d0063e200bb0b31cce4b4a
Patrik Pihlström <patrik.pihlstrom@gmail.com>
2019-06-30 18:09:14 -0500

    implemented getJob*FromBody functions

--- src/helpers/job.ts
+++ src/helpers/job.ts
@@ -3,15 +3,53 @@ import {JobRequest} from '../models/job';
 import {JobUpdateInput, JobWhereInput, JobWhereUniqueInput} from '../../prisma/generated/prisma-client';
 
 export function getJobWhereInput(req: Request): JobWhereInput {
-	return null;
+	return getJobWhereInputFromBody(req.body);
+}
+
+export function getJobWhereInputFromBody(body: any): JobWhereInput {
+	let input = {};
+	for (let key in body) {
+		if (!body.hasOwnProperty(key)) {
+			continue;
+		}
+
+		input[key] = body[key];
+	}
+
+	return input;
 }
 
 export function getJobWhereUniqueInput(req: Request): JobWhereUniqueInput {
-	return null;
+	return getJobWhereUniqueInputFromBody(req.body);
+}
+
+export function getJobWhereUniqueInputFromBody(body: any): JobWhereUniqueInput {
+	if (body.hasOwnProperty('id') && typeof body.id == 'string' || typeof body.id == 'number') {
+		return {id: body.id};
+	}
+
+	return {id: null};
 }
 
 export function getJobUpdateInput(req: Request): JobUpdateInput {
-	return null;
+	return getJobUpdateInputFromBody(req.body);
+}
+
+export function getJobUpdateInputFromBody(body: any): JobUpdateInput {
+	let result = {};
+	for (let key in body) {
+		if (!body.hasOwnProperty(key)) {
+			continue;
+		}
+
+		if (['site', 'driver', 'status', 'container', 'log'].indexOf(key) >= 0) {
+			if (typeof body[key] == 'string' || typeof body[key] == 'number') {
+				result[key] = body[key];
+			}
+		}
+	}
+
+	return result;
 }
 
 function getDriversAndSites(body: any) {
--- test/unit/helpers/job.test.ts
+++ test/unit/helpers/job.test.ts
@@ -17,3 +17,144 @@ describe('get job requests from req object', () => {
 		expect(result[0].site).toBe('test');
 	});
 });
+
+describe('get job where input from body', () => {
+	test('returns empty job where unique input if no params are specified', () => {
+		const body = {};
+		const result = helper.getJobWhereUniqueInputFromBody(body);
+		expect(result).toHaveProperty('id');
+		expect(result.id).toBe(null);
+	});
+
+	test('returns empty job where unique input if invalid params are specified', () => {
+		const body = {id: ['test', '123']};
+		const result = helper.getJobWhereUniqueInputFromBody(body);
+		expect(result).toHaveProperty('id');
+		expect(result.id).toBe(null);
+	});
+
+	test('returns valid job where unique input', () => {
+		const body = {id: 'test'};
+		const result = helper.getJobWhereUniqueInputFromBody(body);
+		expect(result).toHaveProperty('id');
+		expect(result.id).toBe('test');
+	});
+});
+
+describe('get job where input from body', () => {
+	test('returns empty job where input if no params are specified', () => {
+		const body = {};
+		const result = helper.getJobWhereInputFromBody(body);
+		expect(result).toEqual({});
+	});
+
+	test('returns job where input with id', () => {
+		const body = {id: 0};
+		const result = helper.getJobWhereInputFromBody(body);
+		expect(result).toHaveProperty('id');
+		expect(result.id).toBe(0);
+	});
+
+	test('returns job where input with id not eq', () => {
+		const body = {id_not: 0};
+		const result = helper.getJobWhereInputFromBody(body);
+		expect(result).toHaveProperty('id_not');
+		expect(result.id_not).toBe(0);
+	});
+
+	test('returns job where input with id in', () => {
+		const body = {id_in: [0, 1, 2, 3]};
+		const result = helper.getJobWhereInputFromBody(body);
+		expect(result).toHaveProperty('id_in');
+		expect(result.id_in).toHaveLength(4);
+		expect(result.id_in).toContain(0);
+		expect(result.id_in).toContain(1);
+		expect(result.id_in).toContain(2);
+		expect(result.id_in).toContain(3);
+	});
+
+	test('returns job where input with status', () => {
+		const body = {status: 'pending'};
+		const result = helper.getJobWhereInputFromBody(body);
+		expect(result).toHaveProperty('status');
+		expect(result.status).toBe('pending');
+	});
+
+	test('returns job where input with status not eq', () => {
+		const body = {status_not: 'pending'};
+		const result = helper.getJobWhereInputFromBody(body);
+		expect(result).toHaveProperty('status_not');
+		expect(result.status_not).toBe('pending');
+	});
+
+	test('returns job where input with status in', () => {
+		const body = {status_in: ['pending', 'starting', 'running', 'done']};
+		const result = helper.getJobWhereInputFromBody(body);
+		expect(result).toHaveProperty('status_in');
+		expect(result.status_in).toHaveLength(4);
+		expect(result.status_in).toContain('pending');
+		expect(result.status_in).toContain('starting');
+		expect(result.status_in).toContain('running');
+		expect(result.status_in).toContain('done');
+	});
+
+	test('returns job where input with driver', () => {
+		const body = {driver: 'chrome'};
+		const result = helper.getJobWhereInputFromBody(body);
+		expect(result).toHaveProperty('driver');
+		expect(result.driver).toBe('chrome');
+	});
+
+	test('returns job where input with driver not eq', () => {
+		const body = {driver_not: 'chrome'};
+		const result = helper.getJobWhereInputFromBody(body);
+		expect(result).toHaveProperty('driver_not');
+		expect(result.driver_not).toBe('chrome');
+	});
+
+	test('returns job where input with driver in', () => {
+		const body = {driver_in: ['chrome', 'firefox']};
+		const result = helper.getJobWhereInputFromBody(body);
+		expect(result).toHaveProperty('driver_in');
+		expect(result.driver_in).toHaveLength(2);
+		expect(result.driver_in).toContain('chrome');
+		expect(result.driver_in).toContain('firefox');
+	});
+
+	test('returns job where input with site', () => {
+		const body = {site: 'test'};
+		const result = helper.getJobWhereInputFromBody(body);
+		expect(result).toHaveProperty('site');
+		expect(result.site).toBe('test');
+	});
+
+	test('returns job where input with site not eq', () => {
+		const body = {site_not: 'test'};
+		const result = helper.getJobWhereInputFromBody(body);
+		expect(result).toHaveProperty('site_not');
+		expect(result.site_not).toBe('test');
+	});
+
+	test('returns job where input with site in', () => {
+		const body = {site_in: ['test', 'annahub']};
+		const result = helper.getJobWhereInputFromBody(body);
+		expect(result).toHaveProperty('site_in');
+		expect(result.site_in).toHaveLength(2);
+		expect(result.site_in).toContain('test');
+		expect(result.site_in).toContain('annahub');
+	});
+});
+
+describe('get job update input from body', () => {
+	test('returns empty obj with invalid params', () => {
+		const body = {};
+		const result = helper.getJobUpdateInputFromBody(body);
+		expect(result).toEqual({});
+	});
+
+	test('only uses string or number params that also exist in the interface', () => {
+		const body = {site: ['test', 'annahub'], status: 'done', foo: 'bar'};
+		const result = helper.getJobUpdateInputFromBody(body);
+		expect(result).toEqual({status: 'done'});
+	});
+});
