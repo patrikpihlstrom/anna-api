8d6f8363f5974757694c2c1ebf698fa84b39de02
Patrik Pihlström <patrik.pihlstrom@gmail.com>
2019-07-01 23:19:24 -0600

    compile & run

--- src/routes.ts
+++ src/routes.ts
@@ -9,4 +9,7 @@ router.post('/push', Job.push);
 router.post('/update', Job.update);
 router.post('/rm', Job.rm);
 
+// TODO: get('/tasks') to get the names of tasks that exist in an optional given namespace
+// TODO: get('/task?task=$task') to get the tasks that exist in a given namespace
+
 export {router};
