afb16a86819157ad63639a302ffe930194f4582e
Patrik Pihlström <patrik.pihlstrom@gmail.com>
2019-10-17 00:05:42 +0200

    remove old jobs

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,24 @@
+import {scheduleJob} from "node-schedule";
+
 let bodyParser = require("body-parser");
 import * as express from "express";
-import { router } from "./routes";
+import {router} from "./routes";
+import {JobWhereInput, prisma} from "../prisma/generated/prisma-client";
 
 const app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use("/", router);
 app.use("/task", router);
-app.use("/graphql", router);
 
 let handle = null;
 if (process.env.NODE_ENV !== "test") {
-  handle = app.listen(5000, () => console.log("Running on http://localhost:5000/"));
+	scheduleJob('* * * * *', async () => {
+		let date = new Date();
+		date.setDate(date.getDate() - 14);
+		await prisma.deleteManyJobs({updatedAt_lt: date});
+	});
+	handle = app.listen(5000, () => console.log("Running on http://localhost:5000/"));
 }
-export { app, handle };
+
+export {app, handle};
