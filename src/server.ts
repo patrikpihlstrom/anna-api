76debacf9d5067e4d322a211e4aa0154beb6ea08
Patrik Pihlström <patrik.pihlstrom@gmail.com>
2019-10-27 23:25:51 +0100

    config vars for job lifetime & a few other things

--- src/server.ts
+++ src/server.ts
@@ -17,33 +17,51 @@ app.use("/", router);
 app.use("/task", router);
 
 let handle = null;
-if (process.env.NODE_ENV !== "test") {
-	scheduleJob('* * * * *', async () => {
-		let date = new Date();
-		date.setDate(date.getDate() - 14);
-		await prisma.deleteManyJobs({updatedAt_lt: date});
-		let jobs = await prisma.jobs({
-			orderBy: 'updatedAt_ASC',
-			where: {
-				status: 'PENDING'
-			},
-			first: 10
-		});
-		jobs.forEach((job) => {
-			try {
-				request.post(config['load_balancer']['endpoint'], {body: job.id}, (error, res, body) => {
-					if (error) {
-						console.error(error);
-						return;
-					}
-					console.log(`statusCode: ${res.statusCode}`);
-					console.log(body);
-				});
-			} catch (Exception) {
-			}
-		});
+
+async function sendJobs() {
+	let first = 16;
+	if (config.hasOwnProperty('job') && config['job'].hasOwnProperty('first')) {
+		first = config['job']['first'];
+	}
+
+	let jobs = await prisma.jobs({
+		orderBy: 'updatedAt_ASC',
+		where: {
+			status: 'PENDING'
+		},
+		first: first
 	});
-	handle = app.listen(5000, () => console.log("Running on http://localhost:5000/"));
+
+	jobs.forEach((job) => {
+		try {
+			request.post(config['load_balancer']['endpoint'], {body: job.id}, (error, res, body) => {
+				if (error) {
+					console.error(error);
+					return;
+				}
+				console.log(`statusCode: ${res.statusCode}`);
+				console.log(body);
+			});
+		} catch (e) {
+			console.error(e.toString());
+		}
+	});
 }
+
+scheduleJob('* * * * *', async () => {
+	let date = new Date();
+	let jobLife = 4;
+	if (config.hasOwnProperty('job') && config['job'].hasOwnProperty('life')) {
+		jobLife = config['job']['life'];
+	}
+
+	date.setDate(date.getDate() - jobLife);
+	await prisma.deleteManyJobs({updatedAt_lt: date});
+
+	if (config.hasOwnProperty('load_balancer') && config['load_balancer'].hasOwnProperty('endpoint')) {
+		await sendJobs();
+	}
+});
+handle = app.listen(5000, () => console.log("Running on http://localhost:5000/"));
 
 export {app, handle};
